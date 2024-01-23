e721584bfceb12748a82c5f32f68a1f0a32925c3
Fgc17 <fgcoelho1709@outlook.com>
2024-01-23 04:55:10 +0000

    remove: span classes

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -3,11 +3,12 @@ import {
   type InputProps as HeadlessInputProps,
 } from "@headlessui/react";
 import { clsx } from "clsx";
-import { useField, useFormContext } from "./Form";
+import { useFormContext } from "./Form";
 import { MaskType, formatWithMask } from "./_shared/utils/formatWithMask";
 import { ButtonSpinner, LoadingSpinner } from "../Spinners";
 import { Controller } from "react-hook-form";
 import { Span } from "./Span";
+import { useField } from "./Field";
 
 const dateTypes = ["date", "datetime-local", "month", "time", "week"];
 type DateType = (typeof dateTypes)[number];
@@ -26,24 +27,6 @@ const webkitCss = [
   "[&::-webkit-datetime-edit-millisecond-field]:p-0",
   "[&::-webkit-datetime-edit-meridiem-field]:p-0",
 ];
-
-export const inputSpanClasses = clsx([
-  // Basic layouts
-  `relative block w-full`,
-
-  // Background color + shadow applied to inset pseudo element, so shadow blends with border in light mode
-  `before:absolute before:inset-px before:rounded-[calc(theme(borderRadius.lg)-1px)] before:bg-white before:shadow`,
-
-  // Focus ring
-  `after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-inset after:ring-transparent 
-  focus-within:after:ring-2 has-[[data-invalid]]:after:ring-red-500 focus-within:after:ring-blue-500`,
-
-  // Disabled state
-  `has-[[data-disabled]]:opacity-50 before:has-[[data-disabled]]:bg-zinc-950/5 before:has-[[data-disabled]]:shadow-none`,
-
-  // Invalid state
-  `before:has-[[data-invalid]]:shadow-red-500/10`,
-]);
 
 export const inputClasses = clsx(
   // Basic layout
