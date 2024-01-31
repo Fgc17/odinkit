083245ee16ba2d83356e27070e715fa7a71f33af
Fgc17 <fgcoelho1709@outlook.com>
2024-01-31 06:21:43 +0000

    Refactor radio component and field variant styles

--- src/components/Form/Field.tsx
+++ src/components/Form/Field.tsx
@@ -55,22 +55,6 @@ const fieldVariants = {
     // With description
     "[&_[data-slot=label]]:has-[[data-slot=description]]:font-medium"
   ),
-  radio: clsx(
-    // Base layout
-    "grid grid-cols-[1.125rem_1fr] items-center gap-x-4 gap-y-1 sm:grid-cols-[1rem_1fr]",
-
-    // Control layout
-    "[&>[data-slot=control]]:col-start-1 [&>[data-slot=control]]:row-start-1 [&>[data-slot=control]]:justify-self-center",
-
-    // Label layout
-    "[&>[data-slot=label]]:col-start-2 [&>[data-slot=label]]:row-start-1 [&>[data-slot=label]]:justify-self-start",
-
-    // Description layout
-    "[&>[data-slot=description]]:col-start-2 [&>[data-slot=description]]:row-start-2",
-
-    // With description
-    "[&_[data-slot=label]]:has-[[data-slot=description]]:font-medium"
-  ),
 };
 
 const fieldGroupVariants = {
@@ -203,6 +187,7 @@ export function _ODINKIT_INTERNAL_Field<Fields extends FieldValues>({
   className,
   enableAsterisk,
   variant = "default",
+  children,
   ...props
 }: FieldProps<Fields>) {
   const form = useFormContext();
