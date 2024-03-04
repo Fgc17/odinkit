74594576e3ec0119eafcf9d5059b55a58e2cfb1f
gcmarcello <marcellogcoelho@outlook.com>
2024-03-04 21:52:50 +0000

    Delete unused colors.ts file and import useEffect in format.ts

---
+++ src/components/Date.tsx
@@ -0,0 +1,31 @@
+// client
+"use client";
+
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import { useEffect, useState } from "react";
+import { ButtonSpinner } from "./Spinners";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export function Date({
+  date,
+  format,
+  localTime = false,
+}: {
+  date: Date;
+  format: string;
+  localTime?: boolean;
+}) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  return isClient ? (
+    dayjs(date).utc(localTime).format(format)
+  ) : (
+    <ButtonSpinner />
+  );
+}
