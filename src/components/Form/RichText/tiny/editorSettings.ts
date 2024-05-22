94e3a5c1ea7c645d87a4ae4d61896b68bc5921a1
gcmarcello <marcellogcoelho@outlook.com>
2024-05-22 00:27:30 +0000

    Add file upload functionality to the rich text editor

---
+++ src/components/Form/RichText/tiny/editorSettings.ts
@@ -0,0 +1,35 @@
+export const editorSettings = {
+  skin: false,
+  promotion: false,
+  language: "pt_BR",
+  plugins: [
+    "advlist",
+    "autolink",
+    "lists",
+    "link",
+    "image",
+    "charmap",
+    "anchor",
+    "searchreplace",
+    "image",
+    "visualblocks",
+    "code",
+    "fullscreen",
+    "insertdatetime",
+    "media",
+    "table",
+    "preview",
+    "help",
+    "wordcount",
+  ],
+  toolbar:
+    "undo redo | blocks | " +
+    "image media link uploadFileButton | bold italic forecolor | alignleft aligncenter " +
+    "alignright alignjustify | bullist numlist outdent indent | " +
+    "removeformat | help",
+  content_style:
+    "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+  height: 400,
+  menubar: true,
+  content_css: false,
+};
