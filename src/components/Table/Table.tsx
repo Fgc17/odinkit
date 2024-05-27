225c16641d0f05825110590f73d89170f07399c7
gcmarcello <marcellogcoelho@outlook.com>
2024-05-27 17:13:12 +0000

    feat: Update Table bottom padding

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -256,7 +256,7 @@ export function Table<Data>({
         >
       }
     >
-      <Form hform={form} className={clsx(pagination && "pb-20 lg:pb-0")}>
+      <Form hform={form} className={clsx(pagination && "pb-4 lg:pb-0")}>
         <div className="flex items-center justify-between gap-3">
           {search && (
             <Field name="globalFilter" className="flex-grow">
