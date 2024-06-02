acbbf14cb90ab2a448b4b0990d9fcffde539ea93
gcmarcello <marcellogcoelho@outlook.com>
2024-06-02 00:15:48 +0000

    fix: remove bottom padding for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar({
   return (
     <nav
       {...props}
-      className={clsx(className, "flex flex-1 items-center gap-4 py-2.5")}
+      className={clsx(className, "flex flex-1 items-center gap-4 pt-2.5")}
     />
   );
 }
