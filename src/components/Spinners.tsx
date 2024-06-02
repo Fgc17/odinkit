adf1c6c2e243c1c5a4ce91c213493685605761c0
gcmarcello <marcellogcoelho@outlook.com>
2024-06-02 00:09:34 +0000

    feat: update spinner color

--- src/components/Spinners.tsx
+++ src/components/Spinners.tsx
@@ -13,12 +13,12 @@ export function LoadingSpinner() {
           cx="12"
           cy="12"
           r="10"
-          stroke="currentColor"
+          stroke="gray"
           strokeWidth="4"
         ></circle>
         <path
           className="opacity-75"
-          fill="currentColor"
+          fill="gray"
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
@@ -26,10 +26,17 @@ export function LoadingSpinner() {
   );
 }
 
-export function ButtonSpinner({ size = "small" }: { size?: keyof typeof sizeVariants }) {
+export function ButtonSpinner({
+  size = "small",
+}: {
+  size?: keyof typeof sizeVariants;
+}) {
   return (
     <div className={`flex ${sizeVariants[size]?.height}`}>
-      <svg className={`${sizeVariants[size]?.width} animate-spin`} viewBox="0 0 24 24">
+      <svg
+        className={`${sizeVariants[size]?.width} animate-spin`}
+        viewBox="0 0 24 24"
+      >
         <circle
           className="opacity-25"
           cx="12"
