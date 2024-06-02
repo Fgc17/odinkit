99c3b1eb74e88d0617a059e7fdfd822ffa4ef42f
gcmarcello <marcellogcoelho@outlook.com>
2024-06-02 15:01:06 +0000

    fix: padding

--- src/components/Layouts/sidebar-stacked-layout.tsx
+++ src/components/Layouts/sidebar-stacked-layout.tsx
@@ -17,9 +17,9 @@ export function SidebarStackedLayout({
   return (
     <div className="relative isolate flex min-h-svh w-full flex-col bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
       {/* Content */}
-      <div className="py-2 lg:flex lg:flex-1 lg:px-2">
+      <div className="lg:flex lg:flex-1 lg:px-2 lg:py-2">
         {sidebar}
-        <div className="grow p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
+        <div className="grow px-6 pb-2 pt-6 lg:rounded-lg lg:bg-white lg:p-10 lg:pb-6 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
           <div className="mx-auto max-w-7xl">{children}</div>
         </div>
       </div>
