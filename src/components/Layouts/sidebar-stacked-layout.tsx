7a6012c4f38ec10a46a3f4b87ae1e48774805596
gcmarcello <marcellogcoelho@outlook.com>
2024-06-07 12:51:01 +0000

    refactor: Update SidebarStackedLayout component styles

--- src/components/Layouts/sidebar-stacked-layout.tsx
+++ src/components/Layouts/sidebar-stacked-layout.tsx
@@ -1,10 +1,4 @@
 "use client";
-import { useState } from "react";
-import { MobileSidebar } from "./components/MobileSidebar";
-import { NavbarItem, NavbarLabel } from "../Navbar";
-import { OpenMenuIcon } from "./components/OpenMenuIcon";
-import { Button } from "../Button";
-import { AdjustmentsHorizontalIcon } from "@heroicons/react/20/solid";
 
 export function SidebarStackedLayout({
   navbar,
@@ -17,9 +11,9 @@ export function SidebarStackedLayout({
   return (
     <div className="relative isolate flex min-h-[calc(100dvh-50px)] w-full flex-col bg-white lg:min-h-[calc(100dvh-46px)] lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
       {/* Content */}
-      <div className="lg:flex lg:flex-1 lg:px-2 lg:py-2">
+      <div className="lg:flex lg:flex-1 lg:px-2 ">
         {sidebar}
-        <div className="grow px-6 pb-2 pt-6 lg:rounded-lg lg:bg-white lg:p-10 lg:pb-6 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
+        <div className="my-2 grow px-6 pb-2 pt-6 lg:rounded-lg lg:bg-white lg:p-10 lg:pb-6 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
           <div className="lg:max-w-content max-w-8xl mx-auto">{children}</div>
         </div>
       </div>
