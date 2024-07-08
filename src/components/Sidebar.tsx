dbce0e6a188e83d7c9a13de4d37ae7268f92efa9
fgc17 <fgcoelho1709@outlook.com>
2024-07-08 19:31:23 +0000

    fix: sidebar header padding

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -53,7 +53,7 @@ export function SidebarHeader({
       {...props}
       className={clsx(
         className,
-        "flex flex-col border-b border-zinc-950/5 p-4 dark:border-white/5 [&>[data-slot=section]+[data-slot=section]]:mt-2.5"
+        "flex flex-col border-b border-zinc-950/5 dark:border-white/5 [&>[data-slot=section]+[data-slot=section]]:mt-2.5"
       )}
     />
   );
@@ -163,7 +163,7 @@ export const SidebarItem = React.forwardRef(function SidebarItem(
 ) {
   let classes = clsx(
     // Base
-    "flex w-full items-center gap-3 rounded-lg px-2 py-2.5 text-left text-base/6 font-medium text-zinc-950 sm:py-2 sm:text-sm/5",
+    "flex w-full items-center text-sm gap-2 rounded-lg px-2 py-2.5 text-left text-base/6 font-medium text-gray-800 sm:py-2 sm:text-sm/5",
     // Leading icon/icon-only
     "data-[slot=icon]:*:size-6 data-[slot=icon]:*:shrink-0 data-[slot=icon]:*:fill-zinc-500 sm:data-[slot=icon]:*:size-5",
     // Trailing icon (down chevron or similar)
