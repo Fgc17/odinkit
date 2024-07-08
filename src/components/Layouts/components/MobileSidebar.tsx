9f848a85cc6c8a8f65dfb855c411ff60e3fa0fd0
fgc17 <fgcoelho1709@outlook.com>
2024-07-08 19:29:36 +0000

    fix: dynamic sidebar children

--- src/components/Layouts/components/MobileSidebar.tsx
+++ src/components/Layouts/components/MobileSidebar.tsx
@@ -46,14 +46,6 @@ export function MobileSidebar({
             )}
           >
             <div className="flex h-full flex-col rounded-lg bg-white shadow-sm ring-1 ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10">
-              <div className="-mb-3 px-4 pt-3">
-                <Headless.CloseButton
-                  as={NavbarItem}
-                  aria-label="Close navigation"
-                >
-                  <CloseMenuIcon />
-                </Headless.CloseButton>
-              </div>
               {children}
             </div>
           </Headless.DialogPanel>
