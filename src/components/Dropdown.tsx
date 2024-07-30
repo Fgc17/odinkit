f846f19062c5a5f843986fbbd2a1f0418dc9d029
gcmarcello <marcellogcoelho@outlook.com>
2024-07-30 19:41:10 +0000

    refactor: Update DropdownMenu anchor to "bottom end"

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -38,7 +38,7 @@ export function DropdownButton<T extends React.ElementType = typeof Button>(
 }
 
 export function DropdownMenu({
-  anchor = "bottom end",
+  anchor = { to: "bottom end" },
   ...props
 }: { anchor?: NonNullable<HeadlessMenuItemsProps["anchor"]> } & Omit<
   HeadlessMenuItemsProps,
