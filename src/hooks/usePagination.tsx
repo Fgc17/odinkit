c7b85b177b69f80ed8eca764ac0788bbc41dacde
fgc17 <fgcoelho1709@outlook.com>
2024-08-20 13:09:10 +0000

    feat: state

--- src/hooks/usePagination.tsx
+++ src/hooks/usePagination.tsx
@@ -1,20 +1,29 @@
+// client
+"use client";
+
+import { useEffect } from "react";
 import { usePaginationUtils } from "./usePaginationUtils";
 import { useSteps } from "./useSteps";
 
 export function usePagination({
-  pageCount: pageCount,
+  startPageCount,
   startPage: startPage = 0,
+  onWalk,
 }: {
-  pageCount: number;
+  startPageCount: number;
   startPage?: number;
   options?: any;
+  onWalk?: (page: number) => void;
 }) {
   const {
     currentStep: currentPageIndex,
+    stepCount: pageCount,
     walk,
     dryWalk,
+    setStepCount,
+    setCurrentStep,
   } = useSteps({
-    stepCount: pageCount,
+    stepCount: startPageCount,
     currentStep: startPage,
   });
 
@@ -23,11 +32,19 @@ export function usePagination({
     currentPageIndex,
   });
 
+  useEffect(() => {
+    if (onWalk) {
+      onWalk(currentPageIndex);
+    }
+  }, [currentPageIndex]);
+
   return {
     currentPageIndex,
     pageCount,
     walk,
     dryWalk,
+    setPageCount: setStepCount,
+    setCurrentPage: setCurrentStep,
     ...paginationUtils,
   };
 }
