7cb89adff34fe4aad55b155c03caae87ca7e5cc6
fgc17 <fgcoelho1709@outlook.com>
2024-08-20 13:08:24 +0000

    feat: videoplayer

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -9,22 +9,28 @@ import {
   SpeakerWaveIcon,
   SpeakerXMarkIcon,
 } from "@heroicons/react/20/solid";
+import clsx from "clsx";
 
 interface VideoPlayerProps {
   title: string;
   videoUrl: string;
-  thumbnailUrl?: string; // Optional thumbnail
+  thumbnailUrl?: string;
+  coverUrl?: string;
+  hideTime?: boolean;
 }
 
 export function VideoPlayer({
   title,
   videoUrl,
   thumbnailUrl,
+  coverUrl,
+  hideTime,
 }: VideoPlayerProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const overlayRef = useRef<HTMLDivElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
+  const [volume, setVolume] = useState(1); // Volume state
+  const [showVolumeSlider, setShowVolumeSlider] = useState(false); // State to show/hide volume slider
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
 
@@ -43,6 +49,23 @@ export function VideoPlayer({
     if (videoRef.current) {
       videoRef.current.muted = !isMuted;
       setIsMuted(!isMuted);
+    }
+  };
+
+  const handleVolumeChange = (value: number) => {
+    const newVolume = value / 100;
+    setVolume(newVolume);
+    if (videoRef.current) {
+      videoRef.current.volume = newVolume;
+      setIsMuted(newVolume === 0);
+    }
+  };
+
+  const handleProgressChange = (value: number) => {
+    if (videoRef.current) {
+      const newTime = (value / 100) * duration;
+      videoRef.current.currentTime = newTime;
+      setCurrentTime(newTime);
     }
   };
 
@@ -86,7 +109,12 @@ export function VideoPlayer({
     }
   }, [videoRef.current?.muted]);
 
-  // UseEffect to track play and pause events
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = isMuted;
+    }
+  }, [isMuted]);
+
   useEffect(() => {
     const videoElement = videoRef.current;
     if (videoElement) {
@@ -96,7 +124,6 @@ export function VideoPlayer({
       videoElement.addEventListener("play", handlePlay);
       videoElement.addEventListener("pause", handlePause);
 
-      // Clean up event listeners on unmount
       return () => {
         videoElement.removeEventListener("play", handlePlay);
         videoElement.removeEventListener("pause", handlePause);
@@ -105,7 +132,7 @@ export function VideoPlayer({
   }, []);
 
   return (
-    <div className="relative aspect-video w-full max-w-2xl overflow-hidden rounded-lg">
+    <div className="relative aspect-video w-full overflow-hidden rounded-lg">
       <div className="absolute inset-x-0 top-0 bg-gradient-to-b from-black/50 to-transparent p-4 text-lg text-white">
         <div className="line-clamp-1">{title}</div>
       </div>
@@ -119,16 +146,30 @@ export function VideoPlayer({
       >
         <source src={videoUrl} type="video/mp4" />
       </video>
-      <div
-        ref={overlayRef}
-        className="absolute inset-0 z-10"
-        style={{ background: "transparent" }}
-      ></div>
+      {currentTime != 0 && (
+        <div
+          className="absolute inset-0 z-10 bg-transparent bg-contain"
+          onClick={togglePlay}
+        ></div>
+      )}
+      {currentTime === 0 && coverUrl && (
+        <div
+          className="absolute inset-0 z-30 cursor-pointer bg-contain"
+          style={{
+            backgroundImage: `url(${coverUrl})`,
+          }}
+          onClick={togglePlay}
+        ></div>
+      )}
       <div className="absolute inset-x-0 bottom-0 z-20 grid gap-2 bg-gradient-to-b from-transparent to-black/50">
         <div className="mb-0.5 px-3">
-          <Slider progress={(currentTime / duration) * 100} />
+          <Slider
+            progress={(currentTime / duration) * 100}
+            onSlide={handleProgressChange}
+            className="bg-amber-600"
+          />
         </div>
-        <div className="flex items-center gap-3 p-3 pt-0 text-white [&_svg]:text-white">
+        <div className="relative flex items-center gap-3 p-3 pt-0 text-white [&_svg]:text-white">
           <Button onClick={togglePlay}>
             {isPlaying ? (
               <PauseIcon className="h-5 fill-white" />
@@ -136,17 +177,34 @@ export function VideoPlayer({
               <PlayIcon className="h-5 fill-white" />
             )}
           </Button>
-          <Button onClick={toggleMute}>
-            {isMuted ? (
-              <SpeakerXMarkIcon className="h-5 fill-white" />
-            ) : (
-              <SpeakerWaveIcon className="h-5 fill-white" />
+          <div
+            className="relative flex items-center gap-2"
+            onMouseEnter={() => setShowVolumeSlider(true)}
+            onMouseLeave={() => setShowVolumeSlider(false)}
+          >
+            <Button onClick={toggleMute}>
+              {isMuted ? (
+                <SpeakerXMarkIcon className="h-5 fill-white" />
+              ) : (
+                <SpeakerWaveIcon className="h-5 fill-white" />
+              )}
+            </Button>
+            {showVolumeSlider && (
+              <div className="w-32">
+                <Slider
+                  progress={volume * 100}
+                  onSlide={handleVolumeChange}
+                  className="bg-slate-500"
+                />
+              </div>
             )}
-          </Button>
-          <div className="text-sm">
-            {formatTime(currentTime)} /{" "}
-            {duration ? formatTime(duration) : "--:--"}
           </div>
+          {!hideTime && (
+            <div className="text-sm">
+              {formatTime(currentTime)} /{" "}
+              {duration ? formatTime(duration) : "--:--"}
+            </div>
+          )}
           <Button
             onClick={handleFullscreenChange}
             className="ml-auto hover:bg-black/50"
@@ -161,15 +219,36 @@ export function VideoPlayer({
 
 interface SliderProps {
   progress: number;
+  onSlide: (value: number) => void;
+  className?: string;
 }
 
-export function Slider({ progress }: SliderProps) {
+export function Slider({ progress, onSlide, className }: SliderProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+    onSlide(value); // Call the onSlide function passed from the parent component
+  };
+
   return (
-    <div className="flex flex-auto cursor-pointer rounded-full bg-slate-100">
-      <div
-        className="h-1 rounded-l-full rounded-r-[1px] bg-indigo-600"
-        style={{ width: `${progress}%` }}
-      ></div>
+    <div className="relative flex w-full">
+      <input
+        type="range"
+        min="0"
+        max="100"
+        step="0.1"
+        value={progress || 0}
+        onChange={handleChange}
+        className="absolute inset-0 w-full cursor-pointer opacity-0"
+      />
+      <div className="flex flex-auto cursor-pointer rounded-full bg-slate-100">
+        <div
+          className={clsx(
+            "h-1 rounded-l-full rounded-r-[1px]",
+            className ?? "bg-indigo-600"
+          )}
+          style={{ width: `${progress}%` }}
+        ></div>
+      </div>
     </div>
   );
 }
